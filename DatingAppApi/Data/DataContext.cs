4803c185ad42985e7872ef43e6ed57e0814b8581
haris-begluk <harisbegluk@gmail.com>
2018-11-02 20:03:09 +0100

    InitialCommit

---
+++ DatingAppApi/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore; 
+using DatingAppApi.Models;
+namespace DatingAppApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        } 
+        public DbSet<Value> Values { get; set; }
+    }
+}
