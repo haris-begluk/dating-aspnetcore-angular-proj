411f60d45dcadbee98bc4ae5ee2d6c4d488ec3de
haris-begluk <harisbegluk@gmail.com>
2018-11-10 18:49:19 +0100

    Adding the register method to the Auth service

--- DatingAppSPA/src/app/register/register.component.ts
+++ DatingAppSPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,14 +7,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() {}
+  constructor(private auth: AuthService) {}
 
   ngOnInit() {}
   register() {
+    this.auth.register(this.model).subscribe(
+      () => {
+        console.log('registration successful');
+      },
+      error => {
+        console.log(error);
+      }
+    );
     console.log(this.model);
   }
   cancel() {
