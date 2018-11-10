e43c119e981cb418619d309fa361295d3ef212d5
haris-begluk <harisbegluk@gmail.com>
2018-11-10 18:28:34 +0100

    Component communication child to parent using output properties

--- DatingAppSPA/src/app/home/home.component.ts
+++ DatingAppSPA/src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(
@@ -27,5 +27,8 @@ export class HomeComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
