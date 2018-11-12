b1f347493b277364c5b342100af798abe3adf178
haris-begluk <harisbegluk@gmail.com>
2018-11-12 03:11:11 +0100

    Handling errors in Angular correction

--- DatingAppApi/Controllers/AuthController.cs
+++ DatingAppApi/Controllers/AuthController.cs
@@ -42,7 +42,7 @@ namespace DatingAppApi.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer sad no!");
+            // throw new Exception("Computer sad no!");
             //Checking is user stored in database
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
