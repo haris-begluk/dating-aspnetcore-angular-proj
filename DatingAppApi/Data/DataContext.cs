00d58fe8283f68cef65cbde200212e3ac2aec6d6
haris-begluk <harisbegluk@gmail.com>
2018-12-22 21:06:03 +0100

    Extending user model and exploring EF relationships

--- DatingAppApi/Data/DataContext.cs
+++ DatingAppApi/Data/DataContext.cs
@@ -1,14 +1,15 @@
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
 using DatingAppApi.Models;
 namespace DatingAppApi.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options): base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
-        } 
-        public DbSet<Value> Values { get; set; } 
-        public DbSet<User> Users { get; set; }	
+
+        }
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
