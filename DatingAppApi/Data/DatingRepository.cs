f857c73d6850081176f45cc944a37be1f8599cdb
haris-begluk <harisbegluk@gmail.com>
2018-12-27 16:34:25 +0100

    Adding the set main photo functionality to the api

--- DatingAppApi/Data/DatingRepository.cs
+++ DatingAppApi/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingAppApi.Models;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,12 @@ namespace DatingAppApi.Data
         public void Delete<T>(T entity) where T : class
         {
             _context.Remove(entity);
+        }
+
+        public async Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            return await _context.Photos.Where(u => u.UserId == userId)
+            .FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public async Task<Photo> GetPhoto(int id)
