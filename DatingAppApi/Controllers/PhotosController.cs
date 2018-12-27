59a7dc8d3627d71fe1037fe3fbb006bd0df21c3b
haris-begluk <harisbegluk@gmail.com>
2018-12-27 20:32:51 +0100

    Adding the delete photo option to the api

--- DatingAppApi/Controllers/PhotosController.cs
+++ DatingAppApi/Controllers/PhotosController.cs
@@ -97,6 +97,8 @@ namespace DatingAppApi.Controllers
             var photoFromRepo = await _repo.GetPhoto(id);
             if (photoFromRepo.IsMain)
                 return BadRequest("This is alredy the main photo!");
+
+
             var currentMainPhoto = await _repo.GetMainPhotoForUser(userId);
             currentMainPhoto.IsMain = false;
             photoFromRepo.IsMain = true;
@@ -105,6 +107,38 @@ namespace DatingAppApi.Controllers
 
             return BadRequest("Could not set photo to main!");
 
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var user = await _repo.GetUser(userId);
+            if (!user.Photos.Any(p => p.Id == id))
+                return Unauthorized();
+            var photoFromRepo = await _repo.GetPhoto(id);
+            if (photoFromRepo.IsMain)
+                return BadRequest("You cannot delete your main photo!");
+            if (photoFromRepo.PublicId != null)
+            {
+                var deleteParams = new DeletionParams(photoFromRepo.PublicId);
+                var result = _cloudinary.Destroy(deleteParams);
+                if (result.Result == "ok")
+                {
+                    _repo.Delete(photoFromRepo);
+                }
+            }
+            if (photoFromRepo.PublicId == null)
+            {
+                _repo.Delete(photoFromRepo);
+            }
+
+            if (await _repo.SaveAll())
+            {
+                return Ok();
+            }
+            return BadRequest("Faild to delete the photo!");
         }
     }
 }
