41b8dc3bfcde191970fcc0503e7f45e0eaf0f43d
haris-begluk <harisbegluk@gmail.com>
2018-12-30 15:59:56 +0100

    Start implementing reactive form for registration

--- DatingAppSPA/src/app/app.module.ts
+++ DatingAppSPA/src/app/app.module.ts
@@ -1,36 +1,36 @@
-import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
-import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { MemberListResolver } from './_resolvers/member-list.resolver';
-import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { MemberCardComponent } from './members/member-card/member-card.component';
-import { AuthGuard } from './_guards/auth.guard';
-import { AlertifyService } from './services/alertify.service';
-import { AuthService } from './services/auth.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './services/error.inteceptor';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
-import { UserService } from './services/user.service';
-import { JwtModule } from '@auth0/angular-jwt';
-import { NgxGalleryModule } from 'ngx-gallery';
-import { MemberEditResolver } from './_resolvers/member-edit.resolver';
-import { FileUploadModule } from 'ng2-file-upload';
+import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
+import { MemberEditComponent } from "./members/member-edit/member-edit.component";
+import { MemberListResolver } from "./_resolvers/member-list.resolver";
+import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { MemberCardComponent } from "./members/member-card/member-card.component";
+import { AuthGuard } from "./_guards/auth.guard";
+import { AlertifyService } from "./services/alertify.service";
+import { AuthService } from "./services/auth.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
+import { ErrorInterceptorProvider } from "./services/error.inteceptor";
+import { BsDropdownModule, TabsModule } from "ngx-bootstrap";
+import { MemberListComponent } from "./members/member-list/member-list.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./routes";
+import { UserService } from "./services/user.service";
+import { JwtModule } from "@auth0/angular-jwt";
+import { NgxGalleryModule } from "ngx-gallery";
+import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
+import { FileUploadModule } from "ng2-file-upload";
 
 export function tokenGetter() {
-  return localStorage.getItem('token');
+  return localStorage.getItem("token");
 }
 
 @NgModule({
@@ -51,6 +51,7 @@ export function tokenGetter() {
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     NgxGalleryModule,
     FileUploadModule,
     BsDropdownModule.forRoot(),
@@ -59,8 +60,8 @@ export function tokenGetter() {
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
-        whitelistedDomains: ['localhost:5000'],
-        blacklistedRoutes: ['localhost:5000/api/auth']
+        whitelistedDomains: ["localhost:5000"],
+        blacklistedRoutes: ["localhost:5000/api/auth"]
       }
     })
   ],
--- DatingAppSPA/src/app/register/register.component.ts
+++ DatingAppSPA/src/app/register/register.component.ts
@@ -1,32 +1,41 @@
-import { AlertifyService } from './../services/alertify.service';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AlertifyService } from "./../services/alertify.service";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private auth: AuthService, private alertify: AlertifyService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
+  }
   register() {
-    this.auth.register(this.model).subscribe(
-      () => {
-        this.alertify.success('Registration successful.');
-      },
-      error => {
-        this.alertify.error(error);
-      }
-    );
+    console.log(this.registerForm.value);
+    // this.auth.register(this.model).subscribe(
+    //   () => {
+    //     this.alertify.success("Registration successful.");
+    //   },
+    //   error => {
+    //     this.alertify.error(error);
+    //   }
+    // );
     // console.log(this.model);
   }
   cancel() {
     this.cancelRegister.emit(false);
-    this.alertify.warning('Cancelled.');
+    this.alertify.warning("Cancelled.");
   }
 }
