d0826ac3f6492c7a749d3a41c31ba703de75e9ec
haris-begluk <harisbegluk@gmail.com>
2018-12-30 19:06:01 +0100

    Updating the register method in the API

--- DatingAppApi/Helpers/AutoMapperProfiles.cs
+++ DatingAppApi/Helpers/AutoMapperProfiles.cs
@@ -30,6 +30,7 @@ namespace DatingAppApi.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
