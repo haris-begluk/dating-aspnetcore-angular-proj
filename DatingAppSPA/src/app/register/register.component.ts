8d63bf25a96bb1f90d92e2ab9e8e9fd955d9981d
haris-begluk <harisbegluk@gmail.com>
2018-12-30 19:18:43 +0100

    Completing the registration implementation

--- DatingAppSPA/src/app/register/register.component.ts
+++ DatingAppSPA/src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { Router } from "@angular/router";
 import { AlertifyService } from "./../services/alertify.service";
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { AuthService } from "../services/auth.service";
@@ -9,6 +10,7 @@ import {
 } from "@angular/forms";
 import { PARAMETERS } from "@angular/core/src/util/decorators";
 import { BsDatepickerConfig } from "ngx-bootstrap";
+import { User } from "../_models/user";
 
 @Component({
   selector: "app-register",
@@ -17,14 +19,15 @@ import { BsDatepickerConfig } from "ngx-bootstrap";
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor(
     private auth: AuthService,
     private alertify: AlertifyService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -61,16 +64,22 @@ export class RegisterComponent implements OnInit {
       : { mismatch: true };
   }
   register() {
-    console.log(this.registerForm.value);
-    // this.auth.register(this.model).subscribe(
-    //   () => {
-    //     this.alertify.success("Registration successful.");
-    //   },
-    //   error => {
-    //     this.alertify.error(error);
-    //   }
-    // );
-    // console.log(this.model);
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.auth.register(this.user).subscribe(
+        () => {
+          this.alertify.success("Registration successful.");
+        },
+        error => {
+          this.alertify.error(error);
+        },
+        () => {
+          this.auth.login(this.user).subscribe(() => {
+            this.router.navigate(["/members"]);
+          });
+        }
+      );
+    }
   }
   cancel() {
     this.cancelRegister.emit(false);
--- DatingAppSPA/src/app/services/auth.service.ts
+++ DatingAppSPA/src/app/services/auth.service.ts
@@ -1,20 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { environment } from 'src/environments/environment';
-import { User } from '../_models/user';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject } from "rxjs";
+import { map } from "rxjs/operators";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { environment } from "src/environments/environment";
+import { User } from "../_models/user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
-  baseUrl = environment.apiUrl + 'auth/';
+  baseUrl = environment.apiUrl + "auth/";
   jwtHelper = new JwtHelperService();
   decodedToken: any;
   currentUser: User;
-  photoUrl = new BehaviorSubject<string>('../../assets/user.jpg');
+  photoUrl = new BehaviorSubject<string>("../../assets/user.jpg");
   currentPhotoUrl = this.photoUrl.asObservable();
 
   constructor(private http: HttpClient) {}
@@ -23,12 +23,12 @@ export class AuthService {
   }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'login', model).pipe(
+    return this.http.post(this.baseUrl + "login", model).pipe(
       map((response: any) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user.user));
-          localStorage.setItem('token', user.token);
+          localStorage.setItem("user", JSON.stringify(user.user));
+          localStorage.setItem("token", user.token);
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
           this.currentUser = user.user;
           this.changeMemberPhoto(this.currentUser.photoUrl);
@@ -36,11 +36,11 @@ export class AuthService {
       })
     );
   }
-  register(model: any) {
-    return this.http.post(this.baseUrl + 'register', model);
+  register(user: User) {
+    return this.http.post(this.baseUrl + "register", user);
   }
   loggedIn() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     return !this.jwtHelper.isTokenExpired(token);
   }
 }
