09c92a6f3219430346c69795d2831d14d0ac909d
haris-begluk <harisbegluk@gmail.com>
2018-12-31 14:05:40 +0100

    Adding age filtering to the API

--- DatingAppApi/Helpers/UserParams.cs
+++ DatingAppApi/Helpers/UserParams.cs
@@ -12,5 +12,7 @@ namespace DatingAppApi.Helpers
         }
         public int UserId { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
