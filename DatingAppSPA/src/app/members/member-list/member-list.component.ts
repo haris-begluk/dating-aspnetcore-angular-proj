a9bd2ca12dfa5e3f86414a3b7d4014ae9cf50972
haris-begluk <harisbegluk@gmail.com>
2018-12-31 13:29:17 +0100

    Adding filter functionality to the SPA

--- DatingAppSPA/src/app/members/member-list/member-list.component.ts
+++ DatingAppSPA/src/app/members/member-list/member-list.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../../_models/user';
-import { AlertifyService } from './../../services/alertify.service';
-import { UserService } from './../../services/user.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../../_models/user";
+import { AlertifyService } from "./../../services/alertify.service";
+import { UserService } from "./../../services/user.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-member-list',
-  templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  selector: "app-member-list",
+  templateUrl: "./member-list.component.html",
+  styleUrls: ["./member-list.component.css"]
 })
 export class MemberListComponent implements OnInit {
   users: User[];
@@ -19,17 +19,7 @@ export class MemberListComponent implements OnInit {
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data["users"].result;
     });
-  }
-  loadUsers() {
-    this.userService.getUsers().subscribe(
-      (users: User[]) => {
-        this.users = users;
-      },
-      error => {
-        this.alertify.error(error);
-      }
-    );
   }
 }
