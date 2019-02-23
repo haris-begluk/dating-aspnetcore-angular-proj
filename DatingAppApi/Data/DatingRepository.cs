babb496653dc08035e2c086a7aa75dadcbd58fd6
haris-begluk <harisbegluk@gmail.com>
2019-02-23 15:23:22 +0100

    Get and create message methods added to API

--- DatingAppApi/Data/DatingRepository.cs
+++ DatingAppApi/Data/DatingRepository.cs
@@ -108,5 +108,20 @@ namespace DatingAppApi.Data
         {
             return await _context.SaveChangesAsync() > 0;
         }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public Task<PagedList<Message>> GetMessagesForUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DatingAppApi/Helpers/AutoMapperProfiles.cs
+++ DatingAppApi/Helpers/AutoMapperProfiles.cs
@@ -31,6 +31,8 @@ namespace DatingAppApi.Helpers
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<UserForRegisterDto, User>();
+            CreateMap<MessageForCreationDto, Message>().ReverseMap();
+
         }
     }
 }
